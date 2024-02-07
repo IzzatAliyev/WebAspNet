60a774aff53f7a52ae1e4cf34e3fcc8d5388407e
Izzat Aliyev <izzat@izzats-mbp.speedport.ip>
2024-02-07 18:39:46 +0100

    added stylecop to test namespace

--- Web.Test/UnitTest1.cs
+++ Web.Test/UnitTest1.cs
@@ -1,12 +1,23 @@
+// Copyright (c) IUA. All rights reserved.
+
 namespace Web.Test;
 
+/// <summary>
+/// Tests.
+/// </summary>
 public class Tests
 {
+    /// <summary>
+    /// Setup.
+    /// </summary>
     [SetUp]
     public void Setup()
     {
     }
 
+    /// <summary>
+    /// Test 1.
+    /// </summary>
     [Test]
     public void Test1()
     {
